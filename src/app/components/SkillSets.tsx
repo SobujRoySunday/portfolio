1922f9b67d09cbc36407f47a1bd3847db03f11da
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-01-09 10:37:10 +0530

    skillset will expand on row in large screens

--- src/app/components/SkillSets.tsx
+++ src/app/components/SkillSets.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillSets = () => {
   return (
-    <section className="flex flex-col text-3xl font-extralight items-center w-screen tracking-widest py-10 gap-4 bg-gray-900">
+    <section className="flex flex-col lg:flex-row text-3xl font-extralight justify-center lg:justify-around items-center w-screen tracking-widest py-10 lg:py-20 gap-4 bg-gray-900">
       <p className='hover:text-teal-400 transition-all'>DEVELOPER</p>
       <p className='hover:text-teal-400 transition-all'>FITNESS</p>
       <p className='hover:text-teal-400 transition-all'>SINGING</p>
