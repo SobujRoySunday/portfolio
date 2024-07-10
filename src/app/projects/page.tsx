8e40074482691dfdd72696619caab569097b6926
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-07-10 13:07:40 +0530

    passing only the url in axios function

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import React from 'react'
 
 const page = async () => {
-  const projects: Project[] = (await axios.get(`${process.env.BASE_URL}/api/get-all-projects`)).data.data;
+  const projects: Project[] = (await axios.get(`/api/get-all-projects`)).data.data;
 
   return (
     <main className="flex flex-col items-center justify-between w-[80%] mx-auto mt-10">
